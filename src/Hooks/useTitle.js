e49f3f85d8da01d5a2533633548e00c5edbabbdd
Shahinur2010 <shahinurmony4@gmail.com>
2023-06-07 12:25:59 +0600

    website title set up complete

--- src/Hooks/useTitle.js
+++ src/Hooks/useTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 
 const useTitle = title => {
     useEffect(()=>{
-        document.title = `${title} | Toys Hub`;
+        document.title = `${title} | Drawing School`;
     }, [title])
 };
 
