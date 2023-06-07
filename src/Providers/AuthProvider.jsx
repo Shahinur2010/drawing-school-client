df37821941e7cb8f2acdd9ed652e8ce70271858e
Shahinur2010 <shahinurmony4@gmail.com>
2023-06-07 12:16:53 +0600

    authprovider added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
-import app from '../../firebase/firebase.config';
+import app from '../firebase/firebase.config';
 
 
 export const AuthContext = createContext(null);
