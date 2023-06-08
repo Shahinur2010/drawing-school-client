b99bf50f545edd8092a212afd76bc50bc6c3a3d4
Shahinur2010 <shahinurmony4@gmail.com>
2023-06-08 20:03:32 +0600

    theme toggled in home page

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -2,26 +2,33 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import '../Home/Home.css';
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import useTitle from "../../../Hooks/useTitle";
+import { ThemeContext } from '../../../Providers/ThemeProvider/ThemeProvider';
 
 
 const Home = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
     useTitle('Home')
     useEffect(() => {
         AOS.init({ duration: 1000 });
     }, []);
 
     return (
-        <div className="mx-auto">
-            <div className="text-center py-4 text-lg">
+
+<div className={`home ${theme}`}>
+      <button onClick={toggleTheme}>
+        {theme === 'light' ? 'Switch to Dark Theme' : 'Switch to Light Theme'}
+      </button>
+      {/* Rest of your home page content */}
+      <div className="text-center py-4 text-lg">
                 <h1 data-aos="fade-up mt-20">Welcome to Drawing School!</h1>
                 <p data-aos="fade-left" data-aos-delay="200">Scroll down to explore more.</p>
             </div>
             <div data-aos="zoom-in" data-aos-delay="400">
                 {/* Your content */}
             </div>
-        </div>
+    </div>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import {
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
 import AuthProvider from './Providers/Authprovider';
+import { ThemeProvider } from './Providers/ThemeProvider/ThemeProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
